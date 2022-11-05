5212e4bd34f854d3a393f8b8a1794b22dc9d5a26
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-05 16:22:01 +0100

    code added

--- app.js
+++ app.js
@@ -41,3 +41,33 @@ const addBookToLibrary = (e) => {
 	form.reset();
 	display();
 };
+
+function display() {
+
+    books.textContent = '';
+    let index = 0;
+    myLibrary.forEach((book) => {
+        function createBookElement(el, name, content) {
+            const elem = document.createElement(el);
+            div.appendChild(elem);
+            elem.textContent = name;
+            const elem2 = document.createElement(el);
+            elem2.textContent = content;
+            div.appendChild(elem2);
+            }
+
+        const div = document.createElement("div");
+        div.classList.add("container");
+        books.appendChild(div);
+    
+        createBookElement('h4', 'title:', `${book.title}`);
+        createBookElement('h4', 'author:', `${book.author}`);
+        createBookElement('h4', 'pages:', `${book.pages}`);
+    
+        const checkbox = document.createElement("button");
+        if (book.read) {
+          checkbox.textContent = 'read';
+        } else {
+          checkbox.textContent = 'not read';
+        }
+        div.appendChild(checkbox);
