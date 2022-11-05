acaef37c4f11bbdbf3dec2ea67bc308e27ce74f6
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-05 16:20:23 +0100

    code added

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const form = document.querySelector("form");
-const addBookBtn = document.querySelector(".add-book");
-const close = document.querySelector(".close");
-const submit = document.querySelector(".submit");
-const books = document.querySelector(".wrapper-grid");
-const remove = document.querySelectorAll(".remove");
+const form = document.querySelector('form');
+const addBookBtn = document.querySelector('.add-book');
+const close = document.querySelector('.close');
+const submit = document.querySelector('.submit');
+const books = document.querySelector('.wrapper-grid');
+const remove = document.querySelectorAll('.remove');
+
+class Book {
+	constructor(title, author, pages, read) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
+	}
+}
+
+let myLibrary = [];
+
+close.addEventListener('click', () => {
+	form.style.display = 'none';
+});
