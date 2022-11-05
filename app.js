a61d0b37c1275d29d707647e948e67a9e3254000
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-05 16:24:29 +0100

    code added

--- app.js
+++ app.js
@@ -43,31 +43,59 @@ const addBookToLibrary = (e) => {
 };
 
 function display() {
+	books.textContent = '';
+	let index = 0;
+	myLibrary.forEach((book) => {
+		function createBookElement(el, name, content) {
+			const elem = document.createElement(el);
+			div.appendChild(elem);
+			elem.textContent = name;
+			const elem2 = document.createElement(el);
+			elem2.textContent = content;
+			div.appendChild(elem2);
+		}
 
-    books.textContent = '';
-    let index = 0;
-    myLibrary.forEach((book) => {
-        function createBookElement(el, name, content) {
-            const elem = document.createElement(el);
-            div.appendChild(elem);
-            elem.textContent = name;
-            const elem2 = document.createElement(el);
-            elem2.textContent = content;
-            div.appendChild(elem2);
-            }
+		const div = document.createElement('div');
+		div.classList.add('container');
+		books.appendChild(div);
 
-        const div = document.createElement("div");
-        div.classList.add("container");
-        books.appendChild(div);
-    
-        createBookElement('h4', 'title:', `${book.title}`);
-        createBookElement('h4', 'author:', `${book.author}`);
-        createBookElement('h4', 'pages:', `${book.pages}`);
-    
-        const checkbox = document.createElement("button");
-        if (book.read) {
-          checkbox.textContent = 'read';
-        } else {
-          checkbox.textContent = 'not read';
-        }
-        div.appendChild(checkbox);
+		createBookElement('h4', 'title:', `${book.title}`);
+		createBookElement('h4', 'author:', `${book.author}`);
+		createBookElement('h4', 'pages:', `${book.pages}`);
+
+		const checkbox = document.createElement('button');
+		if (book.read) {
+			checkbox.textContent = 'read';
+		} else {
+			checkbox.textContent = 'not read';
+		}
+		div.appendChild(checkbox);
+
+		const removeBookBtn = document.createElement('button');
+		removeBookBtn.classList.add('remove-book-btn');
+		removeBookBtn.textContent = 'remove';
+
+		removeBookBtn.dataset.linkedArray = index;
+		index++;
+		div.appendChild(removeBookBtn);
+
+		removeBookBtn.addEventListener('click', removeBookFromLibrary);
+
+		function removeBookFromLibrary() {
+			let getBookToRemove = removeBookBtn.dataset.linkedArray;
+			myLibrary.splice(parseInt(getBookToRemove), 1);
+			div.remove();
+			display();
+		}
+
+		checkbox.addEventListener('click', () => {
+			if (book.read) {
+				book.read = false;
+				checkbox.textContent = 'not read';
+			} else {
+				book.read = true;
+				checkbox.textContent = 'read';
+			}
+		});
+	});
+}
