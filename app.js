a567f27cea12690bbed789f83ec889fc4171fe39
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-05 16:19:17 +0100

    code added to app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("form");
+const addBookBtn = document.querySelector(".add-book");
+const close = document.querySelector(".close");
+const submit = document.querySelector(".submit");
+const books = document.querySelector(".wrapper-grid");
+const remove = document.querySelectorAll(".remove");
